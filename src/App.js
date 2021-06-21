bc66c8bb9ae795325cbb67bfdee4c69ab3783d32
Somasundaram, Thirumaran (ts291c) <ts291c@att.com>
2021-06-21 11:13:07 -0400

    Consent Management App Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import TermsAndConditions from './components/TermsAndConditions';
+import TermsOfUse from './components/TermsOfUse';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/consent/termsandconditions" component={TermsAndConditions}/>
+        <Route path="/consent/termsofuse" component={TermsOfUse}/>
+      </Switch>
+    </Router>
+   /* <div>
+      <TermsAndConditions/>
+    </div>*/
   );
 }
 
